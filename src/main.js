197bf2ff5518d36cce2f1e557ef58d321466085b
devsoft984 <devsoft984@gmail.com>
2019-10-16 23:56:03 -0400

    add store

--- src/main.js
+++ src/main.js
@@ -1,24 +1,19 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
 import router from './router';
 import axios from 'axios';
 import VueAxios from 'vue-axios'
 import vuetify from './plugins/vuetify';
 import App from './App.vue';
 
-Vue.use(Vuex)
+import {store} from './store'
+
 Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
-let store = new Vuex.Store({
-  state: {
-    brand: []
-  }
-});
 new Vue({
   vuetify,
   store,
   router,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
